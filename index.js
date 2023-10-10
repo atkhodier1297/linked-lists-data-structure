91773b2baec2d25e61ac4fa4344806fe76961bbf
Adam Khodier <khodieradam1297@gmail.com>
2023-10-10 14:34:18 -0400

    array vs hash notes

---
+++ index.js
@@ -0,0 +1,39 @@
+// Arrays vs Hash Tables
+
+// 1. **Space Usage**:
+//    - Arrays use less memory because they only store values.
+//    - Hash tables use more memory because they store both keys and values.
+
+// 2. **Access Speed**:
+//    - Arrays are fast to access with a constant time O(1) because you know the index.
+//    - Hash tables are also typically fast (O(1) on average), but occasional collisions can make them slower in rare cases.
+
+// 3. **Insertion and Deletion**:
+//    - Arrays are slow for insertions and deletions (O(n)) because shifting elements may be needed.
+//    - Hash tables are fast for insertions and deletions (O(1) on average).
+
+// 4. **Order**:
+//    - Arrays are ordered, meaning elements are in a specific sequence.
+//    - Hash tables are unordered, so there's no guaranteed order.
+
+// 5. **Duplicates**:
+//    - Arrays can contain duplicates easily.
+//    - Hash tables can also store duplicates, but handling them may require extra work.
+
+// 6. **Sorting**:
+//    - Arrays can be sorted efficiently.
+//    - Hash tables are not naturally suited for sorting; you'd need to convert them to an array first.
+
+// 7. **Memory Usage**:
+//    - Arrays are memory-efficient when the size is known and fixed.
+//    - Hash tables can waste memory if they're significantly larger than the stored elements.
+
+// 8. **Keys vs. Values**:
+//    - Arrays only store values.
+//    - Hash tables store key-value pairs, useful for mapping relationships.
+
+// 9. **Implementation Complexity**:
+//    - Arrays are straightforward to use and implement.
+//    - Hash tables require more complex implementation and may introduce bugs if not done correctly.
+
+// Our next data structure is Linked Lists
