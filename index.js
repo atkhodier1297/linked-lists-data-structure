079c2928b19fa6ce8850c77e6659ec223e3a354a
Adam Khodier <khodieradam1297@gmail.com>
2023-10-23 21:55:34 -0400

    doubly linked list conversion start

--- index.js
+++ index.js
@@ -175,3 +175,84 @@ console.log(myLinkedList.printList());
 // Lookup is still O(n) but technically its O(n/2)
 // Simplified it's still O(n) but its actually faster
 
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    // O(1)
+    // const newNode = {
+    //   value: value,
+    //   next: null,
+    // };
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  // O(1)
+  prepend(value) {
+    // const newNode = {
+    //   value: value,
+    //   next: null
+    // };
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    // check parameters
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    // traverse our list to find the insert node
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+const myDoublyLinkedList = new DoublyLinkedList(100);
+myDoublyLinkedList.append(200);
+myDoublyLinkedList.append(300);
+myDoublyLinkedList.prepend(400);
+myDoublyLinkedList.insert(2, 500);
+myDoublyLinkedList.remove(2);
+console.log(myDoublyLinkedList.printList());
